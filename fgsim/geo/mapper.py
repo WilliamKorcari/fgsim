f82d9d9825460f1aa541f5a4fea2317e08bf99f8
mova <mova@users.noreply.github.com>
2020-12-23 11:27:38 +0100

    start with the geo subpackage

---
+++ fgsim/geo/mapper.py
@@ -0,0 +1,14 @@
+from typing import TextIO
+
+
+class geomapper:
+    """
+    This class provides a geomapper object, that is used to map hits ton cells.
+    """
+
+    def __init__(self, configfile: TextIO):
+        self.configfile = configfile
+        print(f"Init mappber by {self.configfile}")
+        with open(self.configfile, "r") as f:
+            for line in f:
+                print(line)
