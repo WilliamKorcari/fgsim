898521a172c8439d07ba7e2788c5a92c4a248f64
mova <mova@users.noreply.github.com>
2021-02-04 12:26:56 +0100

    fast data dumping from ak array

--- fgsim/data_dumper.py
+++ fgsim/data_dumper.py
@@ -1,15 +1,17 @@
 # Uproot4 is yet to implement writing...
 import awkward as ak
 import uproot3 as uproot
+import numpy as np
 
 from .config import conf
 
+
 def dump_generated_events(arr: ak.Array):
 
-    fn = f"wd/{conf['tag']}/output.root"
+    fn = f"wd/{conf.tag}/output.root"
+    n = len(arr)
     with uproot.recreate(fn) as file:
-        file["tree1"] = uproot.newtree()
-        for branch in arr.fields:
-            ak0_array = ak.to_awkward0(arr[branch])
-            n = arr[branch].counts
-            file["tree1"].extend({branch: ak0_array, "n": n})
+        file["tree1"] = uproot.newtree(dict(arr.type.type))
+        file["tree1"].extend(
+            {branch: arr[branch] for branch in arr.fields}
+        )
