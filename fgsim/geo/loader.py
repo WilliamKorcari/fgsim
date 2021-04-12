08559ba3b7da448c3b1644261f314e8995510998
mova <mova@users.noreply.github.com>
2021-04-12 16:30:50 +0200

    add loader for the geometry info

---
+++ fgsim/geo/loader.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+from typing import List
+
+import awkward as ak
+import numpy as np
+import uproot
+
+from ..config import conf
+
+# load the root table
+fn = conf["luppath"]
+rf = uproot.open(fn)
+arr = rf["analyzer/tree"].arrays()
+keydf = ak.to_pandas(arr[0])
+
+
+# load the geometry
+
+fngeopic = geoyamlpath.strip('yaml')+'pickle'
+if os.path.isfile(fngeopic):
+    with open(fngeopic, "rb") as f:
+        geoD = pickle.load(f)
+else:
+    with open(geoyamlpath, "r") as f:
+        geoD = yaml.load(f)
+    with open(fngeopic, "wb") as f:
+        pickle.dump(geoD, f)
